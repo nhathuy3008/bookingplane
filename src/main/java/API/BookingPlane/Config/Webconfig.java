40af06da688f634169cf65b2ae47b41c6355d7e7
Tran Nhat Huy <nhathuy1237890@gmail.com>
2025-04-11 18:43:13 +0700

    fix dÃata

--- src/main/java/API/BookingPlane/Config/Webconfig.java
+++ src/main/java/API/BookingPlane/Config/Webconfig.java
@@ -11,7 +11,7 @@ public class Webconfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**") // Áp dụng CORS cho tất cả các endpoint
-                .allowedOrigins("http://localhost:5173") // Cho phép yêu cầu từ frontend React (địa chỉ này là nơi bạn đang chạy React)
+                .allowedOrigins("http://localhost:5173","https://bookingplane.vercel.app") // Cho phép yêu cầu từ frontend React (địa chỉ này là nơi bạn đang chạy React)
                 .allowedMethods("GET", "POST", "PUT", "DELETE") // Các phương thức HTTP được phép
                 .allowedHeaders("*") // Cho phép tất cả các header
                 .allowCredentials(true); // Cho phép cookie và thông tin xác thực khác
